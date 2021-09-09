32b1972f79488810a3c40ed8ea83b2ca44453685
William Aguilar <williamaguilar@misena.edu.co>
2021-09-09 07:48:27 -0500

    add function backform javascript

--- script.js
+++ script.js
@@ -2,26 +2,37 @@ const form = document.getElementById("form")
 const paragraphAfterSend = document.getElementById("paragraph-after-send")
 const buttonAfterSend = document.getElementById("button-after-send")
 
-form.addEventListener('submit', handleSubmit)
-
-function handleSubmit(event){
-    event.preventDefault()
-
-}
-
-const btn = document.getElementById('submit')
-counter = 0
 
 
-btn.addEventListener('click',change,true)
 
-function change(){
-    if(counter==0)
-    {
-        form.classList.add('displaynone')
-        paragraphAfterSend.classList.toggle('disappear')
-        buttonAfterSend.classList.toggle('disappear')
-        counter = 1
+
+    form.addEventListener('submit', handleSubmit)
+
+    function handleSubmit(event){
+        event.preventDefault()
+
+    }
+    const btn = document.getElementById('submit')
+
+    btn.addEventListener('click',change,true)
+
+    function change(){
+        
+            form.classList.add('displaynone')
+            paragraphAfterSend.classList.remove('disappear')
+            buttonAfterSend.classList.remove('disappear')
+        
+
     }
 
-}
+
+    buttonAfterSend.addEventListener('click',backForm,true) 
+
+    function backForm(){
+        paragraphAfterSend.classList.add('disappear')
+        buttonAfterSend.classList.add('disappear')
+        form.classList.remove('displaynone')
+    }
+
+
+
